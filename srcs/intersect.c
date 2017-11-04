0a19caea92488d327d8bde8b25ebf2529cbd9049
Basile BELDAME <basile.beldame@supdeweb-paris.com>
2017-11-04 20:25:36 +0100

    More norm

--- srcs/intersect.c
+++ srcs/intersect.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersect.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbeldame <bbeldame@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/04 20:20:54 by bbeldame          #+#    #+#             */
+/*   Updated: 2017/11/04 20:22:18 by bbeldame         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/rt.h"
 
-float		limit_obj(t_ray ray, t_obj *obj, float dist, t_rt *e)
+float			limit_obj(t_ray ray, t_obj *obj, float dist, t_rt *e)
 {
 	int			u;
 	float		dist2;
@@ -8,44 +20,56 @@ float		limit_obj(t_ray ray, t_obj *obj, float dist, t_rt *e)
 	t_ray		poi;
 
 	dist2 = DIST_MAX;
-	u = 0; 
-	poi = c_ray(vec_add3(ray.pos, vec_scale3(ray.dir, dist)), vec_new3(0,0,0));
-	while(u < obj->limit_nbr && (dist != DIST_MAX && dist != dist2))
+	u = 0;
+	poi = c_ray(vec_add3(ray.pos, vec_scale3(ray.dir, dist)),
+		vec_new3(0, 0, 0));
+	while (u < obj->limit_nbr && (dist != DIST_MAX && dist != dist2))
 	{
-		poi.dir = vec_norme3(vec_sub3( obj->limit[u].pos, poi.pos));
+		poi.dir = vec_norme3(vec_sub3(obj->limit[u].pos, poi.pos));
 		dist2 = intersect_obj_limit(ray, &obj->limit[u], e);
 		norm = norm_limit_plane(&obj->limit[u], poi);
-		if ((norm.x == (obj->limit[u].vector.x * -1) && norm.y == (obj->limit[u].vector.y * -1) && norm.z == (obj->limit[u].vector.z * -1)) || dist == DIST_MAX)
+		if ((norm.x == (obj->limit[u].vector.x * -1) &&
+			norm.y == (obj->limit[u].vector.y * -1) &&
+				norm.z == (obj->limit[u].vector.z * -1)) || dist == DIST_MAX)
 			u++;
 		else
 		{
-			if (norm.x == (obj->limit[u].vector.x) && norm.y == (obj->limit[u].vector.y) && norm.z == (obj->limit[u].vector.z)  && dist2 < obj->max_dist && dist2 < DIST_MAX && dist <= dist2)
+			if (norm.x == (obj->limit[u].vector.x) &&
+				norm.y == (obj->limit[u].vector.y) &&
+				norm.z == (obj->limit[u].vector.z) &&
+				dist2 < obj->max_dist && dist2 < DIST_MAX && dist <= dist2)
 				dist = dist2;
-			else 
+			else
 				dist = DIST_MAX;
 			u++;
 		}
 	}
-	return(dist);
+	return (dist);
 }
 
 float			intersect_obj(t_ray ray, t_obj *obj, t_rt *e)
 {
 	float dist;
+
 	dist = DIST_MAX;
 	if (obj->type == CYLINDER)
-		dist = check_negative_objects(intersect_cylinder(ray, obj), e, ray, obj->max_dist);
+		dist = check_negative_objects(
+			intersect_cylinder(ray, obj), e, ray, obj->max_dist);
 	else if (obj->type == SPHERE)
-		dist = check_negative_objects(intersect_sphere(ray, obj), e, ray, obj->max_dist);
+		dist = check_negative_objects(
+			intersect_sphere(ray, obj), e, ray, obj->max_dist);
 	else if (obj->type == PLANE)
-		dist = check_negative_objects(intersect_plane(ray, obj), e, ray, obj->max_dist);
+		dist = check_negative_objects(
+			intersect_plane(ray, obj), e, ray, obj->max_dist);
 	else if (obj->type == CONE)
-		dist = check_negative_objects(intersect_cone(ray, obj), e, ray, obj->max_dist);
+		dist = check_negative_objects(
+			intersect_cone(ray, obj), e, ray, obj->max_dist);
 	else if (obj->type == PARABOLOID)
-		dist = check_negative_objects(intersect_paraboloid(ray, obj), e, ray, obj->max_dist);
-	if(obj->limit_active == 1 && dist != DIST_MAX)
+		dist = check_negative_objects(
+			intersect_paraboloid(ray, obj), e, ray, obj->max_dist);
+	if (obj->limit_active == 1 && dist != DIST_MAX)
 		return (limit_obj(ray, obj, dist, e));
-	return(dist);
+	return (dist);
 }
 
 float			get_length(t_vec3 v)
@@ -64,7 +88,7 @@ float			get_min_dist(t_rt *e, t_ray ray)
 	min_dist = DIST_MAX;
 	while (i < e->scene.nbr_obj)
 	{
-		dist = intersect_obj(ray, &e->scene.obj[i], e);	
+		dist = intersect_obj(ray, &e->scene.obj[i], e);
 		if (dist < min_dist && e->scene.obj[i].neg != 1)
 		{
 			min_dist = (dist < 0) ? min_dist : dist;
--- srcs/main.c
+++ srcs/main.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbeldame <bbeldame@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/04 20:17:56 by bbeldame          #+#    #+#             */
+/*   Updated: 2017/11/04 20:18:47 by bbeldame         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/rt.h"
 
-int     main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	t_rt	*e;
 
-    if (!(e = (t_rt *)malloc(sizeof(t_rt))))
-        return (0);
-    e->mlx.init = mlx_init();
-    e->scene.nbr_light = 0;
-    e->scene.nbr_obj = 0;
-    e->frame = 0;
-    if (argc >= 2)
-    {
-        parse(e, argc, argv);
-        ft_start_rt(e);
-    }
-    else
-        ft_gtk_start_launcher(e);
-    return (0);
+	if (!(e = (t_rt *)malloc(sizeof(t_rt))))
+		return (0);
+	e->mlx.init = mlx_init();
+	e->scene.nbr_light = 0;
+	e->scene.nbr_obj = 0;
+	e->frame = 0;
+	if (argc >= 2)
+	{
+		parse(e, argc, argv);
+		ft_start_rt(e);
+	}
+	else
+		ft_gtk_start_launcher(e);
+	return (0);
 }
